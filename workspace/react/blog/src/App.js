15752989d49487f24f6e6d78b5095b841a283a9f
Roong-z <107052335+Roong-z@users.noreply.github.com>
2022-07-22 23:43:28 +0900

    H0722-1

--- workspace/react/blog/src/App.js
+++ workspace/react/blog/src/App.js
@@ -3,14 +3,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import $ from 'jquery';
 
 function App() {
 
   let post = '고양이 맛집';
   const [글제목, 제목변경] = useState(['츄르 추천', '캣닢향 중독', '고양이 양말 추천'])
   let logo = 'ReactBlog';
-  let [좋아요숫자, 숫자증가] = useState(0);
-
+  let [첫번째숫자, 첫번째숫자증가] = useState(0);
+  let [두번째숫자, 두번째숫자증가] = useState(0);
+  let [세번째숫자, 세번째숫자증가] = useState(0);
 
   return (
     <div className="App">
@@ -25,7 +27,7 @@ function App() {
             제목정렬.sort();
             제목변경(제목정렬)
           }}>가나다순 정렬</button>
-          <h4>{ 글제목[0] } <span onClick={ () => { 숫자증가(좋아요숫자+1) } }>👍</span> { 좋아요숫자 } </h4>
+          <h4>{ 글제목[0] } <span onClick={ () => { 첫번째숫자증가(첫번째숫자+1) }}>👍</span> { 첫번째숫자 } </h4>
         <p>7월 18일 발행</p>
 
         {/* 카피해서 해야지 확장성 업 */}
@@ -38,13 +40,19 @@ function App() {
 
         </div>
         <div className = "list">
-          <h4>{ 글제목[1] } <span>👍</span> { 좋아요숫자 } </h4>
+          <h4>{ 글제목[1] } <span onClick={ () => { 두번째숫자증가(두번째숫자+1) }}>👍</span> { 두번째숫자 } </h4>
         <p>7월 18일 발행</p>        
         </div>
         <div className = "list">
-          <h4>{ 글제목[2] } <span>👍</span> { 좋아요숫자 } </h4>
+          <h4>{ 글제목[2] } <span onClick={ () => { 세번째숫자증가(세번째숫자+1) }}>👍</span> { 세번째숫자 } </h4>
         <p>7월 18일 발행</p>
         </div>
+
+      <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
     </div>
   );
 }
