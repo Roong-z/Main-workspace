86954b51881e4758382f10d5b9f9faf7437c66c1
Roong-z <cracker1023@gmail.com>
2022-08-16 02:09:36 +0900

    반복문연습

---
+++ workspace/js/homework/구구단.js
@@ -0,0 +1,17 @@
+// for (var i = 1; i < 10; i++) {
+//   console.log(2 * i);
+// }
+
+// for (var i = 1; i < 10; i++) {
+//   console.log(3 * i);
+// }
+
+// for (var i = 1; i < 10; i++) {
+//   console.log(4 * i);
+// }
+
+for (var k = 2; k < 10; k++) {
+  for (var i = 1; i < 10; i++) {
+    console.log(k * i);
+  }
+}
